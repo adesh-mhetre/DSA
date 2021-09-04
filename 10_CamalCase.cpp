e728752cd754f2ef3e40d73850ab1b5a3c4896c8
adesh-mhetre <adeshmhetre143@gmail.com>
2021-09-05 00:38:58 +0530

    removed comment

--- 10_CamalCase.cpp
+++ 10_CamalCase.cpp
@@ -2,13 +2,6 @@
 
 using namespace std;
 
-/*
- * Complete the 'camelcase' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts STRING s as parameter.
- */
-
 int camelcase(string s) {
     int count=1;
     for(int i=0;i<s.length();i++)
--- 7_second_largest_num.cpp
+++ 7_second_largest_num.cpp
@@ -7,8 +7,10 @@ using namespace std;
 int main()
 {
     int n;
+    cout<<"Enter the number of array elements : ";
     cin>>n;
     int arr[n];
+    cout<<"Enter array Elements : ";
     for(int i=0;i<n;i++)
     {
         cin>>arr[i];
